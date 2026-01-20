cbc14625f2b949ea28c978f9b8dbb39ce774ee10
Godsfavour Jesse <godsfavourjesse@gmail.com>
2026-01-20 11:33:38 +0100

    Fluxsend v3.1.1

--- types/socket.ts
+++ types/socket.ts
@@ -1,7 +1,7 @@
 export type WSMessage = 
     | { type: "pair-joined"; roomId: string }
     | { type: "file-offer"; files: FileMeta[]; totalSize: number }
-    | { tyope:  "file-offer-accepted" }
+    | { type:  "file-offer-accepted" }
     | { type: "file-offer-rejected" }
     | { type: "file-chunk-raw"; buffer: ArrayBuffer }
     | { type: "file-complete"; fileId: string }
